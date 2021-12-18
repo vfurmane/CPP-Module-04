07529c5e98a2137b33f99cec580db6dff0a1e043
vfurmane <vfurmane@student.42.fr>
2021-12-18 18:48:58 +0100

    test: add tests for constructors/destructors and clone method

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,11 +6,21 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 12:15:03 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/12/15 12:15:28 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/12/18 18:48:20 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Ice.hpp"
+#include "Cure.hpp"
+
 int	main(void)
 {
+	AMateria	*glacon = new Ice();
+	AMateria	*remede = new Cure();
+	AMateria	*neige = glacon->clone();
+
+	delete neige;
+	delete glacon;
+	delete remede;
 	return 0;
 }
